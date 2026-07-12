3d471464bd36f381d44263ce50efb5bd3e6160cb
Turret2075 <turret207510@gmail.com>
2026-07-12 10:42:55 -0600

    Organized 7421 Prefab assets and tuned 5959 new components

--- Assets/Prefabs/Reefscape/Robots/Mods/MexicoModpack/5959/TitaniumRamsSetpoint.cs
+++ Assets/Prefabs/Reefscape/Robots/Mods/MexicoModpack/5959/TitaniumRamsSetpoint.cs
@@ -6,7 +6,8 @@ namespace Prefabs.Reefscape.Robots.Mods.PrepaTecPack._5959
    [CreateAssetMenu(fileName = "Setpoint", menuName = "Robot/Titanium Rams Setpoint", order = 0)]
    public class TitaniumRamsSetpoint : ScriptableObject
    {
-       [Tooltip("Inches")] public float elevatorHeight;
-         [Tooltip("Degrees")] public float algaeArmAngle;
+      [Tooltip("Inches")] public float elevatorHeight;
+      [Tooltip("Degrees")] public float algaeArmAngle;
+      [Tooltip("Degrees")] public float algaeDescorerArmAngle;
    }
 }
