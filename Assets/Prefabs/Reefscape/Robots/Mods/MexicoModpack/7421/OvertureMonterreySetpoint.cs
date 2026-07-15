ea3bb64ac1a511ff9539b1ef4de47152e9e20347
Turret2075 <turret207510@gmail.com>
2026-07-15 16:17:43 -0600

    added 7421 Monterrey assets (LFS properly configured)

---
+++ Assets/Prefabs/Reefscape/Robots/Mods/MexicoModpack/7421/OvertureMonterreySetpoint.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+namespace Prefabs.Reefscape.Robots.Mods.MexicoModpack._7421
+{
+   [CreateAssetMenu(fileName = "Setpoint", menuName = "Robot/Overture Monterrey Setpoint", order = 0)]
+   public class OvertureMonterreySetpoint : ScriptableObject
+   {
+      [Tooltip("Inches")] public float elevatorHeight;
+      [Tooltip("Degrees")] public float armAngle;
+      [Tooltip("Degrees")] public float endEffectorAngle;
+      [Tooltip("Degrees")] public float climberAngle;
+      [Tooltip("Degrees")] public float clawAngle;
+   }
+}
