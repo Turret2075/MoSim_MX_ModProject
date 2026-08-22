4fa4c07c7c384bf1a338010d43a466bf3c54e374
Turret2075 <turret207510@gmail.com>
2026-08-21 23:32:11 -0600

    PO almost working

---
+++ Assets/Prefabs/Reefscape/Robots/Mods/MexicoModpackWave2/3478/LambotOffseasonSetpoint.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Prefabs.Reefscape.Robots.Mods.Lambot._9978
+{
+    [CreateAssetMenu(fileName = "Setpoint", menuName = "Robot/Lambot Offseason Setpoint", order = 0)]
+    public class LambotOffseasonSetpoint : ScriptableObject
+    {
+        [Tooltip("Deg")] public float armAngle;
+        [Tooltip("Inch")] public float elevatorHeight;
+        [Tooltip("Deg")] public float intakeAngle;
+        [Tooltip("Deg")] public float climberAngle;
+    }
+}
